6d9075cce59f93a063d43978e2c65f73d0b3f81e
msous401 <147604132+msous401@users.noreply.github.com>
2024-01-10 12:43:28 +0530

    Update carbon.py

--- plugins/Tamilans Extra Mods/carbon.py
+++ plugins/Tamilans Extra Mods/carbon.py
@@ -42,8 +42,8 @@ async def carbon_func(_, message):
     await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
     await message.reply_photo(
         photo=carbon,
-        caption="**CᴏᴅᴇᴅᴇD Bʏ @Tamilan_BotsZ**",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝚄𝚂", url="https://t.me/CrazyboyOfficial")]]),                   
+        caption="**CᴏᴅᴇᴅᴇD Bʏ @CrazyboyHere**",
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 𝚄𝚂", url="https://t.me/CrazyboyHere")]]),                   
     )
     await m.delete()
     carbon.close()
