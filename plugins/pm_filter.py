b8aa46c2f48fcb5309da046c7fc5c5865ebbd173
msous401 <147604132+msous401@users.noreply.github.com>
2024-01-12 01:23:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -715,7 +715,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ]
             reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_reply_markup(reply_markup)
-    await query.answer(' ˆ°• Tᴀᴍɪʟᴀɴ BᴏᴛsZ •°ˆ ')
+    await query.answer(' ˆ°• CrazyboyHere •°ˆ ')
 
 
 async def auto_filter(client, msg, spoll=False):
